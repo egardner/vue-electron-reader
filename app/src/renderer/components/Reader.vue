9c979fdbc72a438e03c9cf5d7cdbfe15b66e4f6c
Eric Gardner <gardner.ec@gmail.com>
2017-04-14 19:20:06 -0700

    Basic epub download

--- app/src/renderer/components/Reader.vue
+++ app/src/renderer/components/Reader.vue
@@ -6,9 +6,16 @@
 </template>
 
 <script>
- import NavBar from './NavBar.vue'
+ import https from 'https'
+ import fs from 'fs'
+ import path from 'path'
  import _ from 'lodash'
  import ePub from 'epubjs'
+ import { remote } from 'electron'
+ import NavBar from './NavBar.vue'
+
+ const { app } = remote
+
  export default {
    name: 'reader',
    components: { NavBar },
@@ -47,11 +54,34 @@
      },
      epub () {
        if (!this.epubLink) {
-         return {}
+         return false
        } else {
          return ePub(this.epubLink)
        }
      }
+   },
+   methods: {
+     downloadFile (url, dest, cb) {
+       let file = fs.createWriteStream(dest)
+       https.get(url, function (response) {
+         response.pipe(file)
+         file.on('finish', function () {
+           file.close(cb)  // close() is async, call cb after close completes.
+         })
+       }).on('error', function (err) { // Handle errors
+         fs.unlink(dest) // Delete the file async. (But we don't check the result)
+         if (cb) cb(err.message)
+       })
+     }
+   },
+   mounted () {
+     console.log(app.getName())
+     let fileName = this.epubLink.split('/').pop()
+     let destPath = path.join(app.getPath('userData'), fileName)
+     this.downloadFile(this.epubLink, destPath, () => {
+       // Do something with the file here
+       console.log(`File downloaded to ${destPath}`)
+     })
    }
  }
 </script>
